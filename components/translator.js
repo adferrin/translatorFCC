c52211657239d32b9a7b5ecce9ae298628605079
adferrin <adferrin@icloud.com>
2021-04-23 13:27:54 -0700

    quick fix

--- components/translator.js
+++ components/translator.js
@@ -5,6 +5,23 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    toBritishEnglish(text) {
+        const dict = {...americanOnly, ...americanToBritishSpelling};
+        const titles = americanToBritishTitles;
+        const timeRegex = /([1-9]|1[012]):[0-5][0-9]/g;
+        const translated = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            "toBritish"
+        );
+        if (!translated) {
+            return text;
+        }
+
+        return translated;
+    }
 }
 
 module.exports = Translator;
