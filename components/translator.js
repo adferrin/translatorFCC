a2f05166893324dfc44f0a8bbf89822472eed250
adferrin <adferrin@icloud.com>
2021-04-27 14:36:24 -0700

    functing well

--- components/translator.js
+++ components/translator.js
@@ -45,6 +45,66 @@ class Translator {
         }
         return translated;
     }
-}
+    translate(text, dict, titles, timeRegex, locale) {
+        const lowerText = text.toLowerCase();
+        const matchesMap = {};
+
+        Object.entries(titles).map(([k, v]) => {
+            if (lowerText.includes(k)) {
+                matchesMap[k] = v.charAt(0).toUpperCase() + v.slice(1);
+            }
+        });
+
+        const wordsWithSpace = Object.fromEntries(
+            Object.entries(dict).filter(([k, v]) => k.includes(" "))
+        );
+
+        Object.entries(wordsWithSpace).map(([k, v]) => {
+            if (lowerText.includes(k)) {
+                matchesMap[k] = v;
+            }
+        });
+        
+        lowerText.match(/(\w+([-'])(\w+)?['-]?(\w+))|\w+/g).forEach((word) => {
+            if (dict[word]) matchesMap[word] = dict[word];
+        });
+
+        const matchedTimes = lowerText.match(timeRegex);
+
+        if (matchedTimes) {
+            matchedTimes.map((e) => {
+                if (locale === "toBritish") {
+                    return (matchesMap[e] = e.replace(":", "."));
+                }
+                return (matchesMap[e] = e.replace(".", ":"));
+            });
+        }
+
+        if (Object.keys(matchesMap).length === 0) return null;
+        
+        console.log("matchesMap :>> ", matchesMap);
+        const translation = this.replaceAll(text, matchesMap);
+
+        const translationWithHighlight = this.replaceAllWithHighlight(
+            text,
+            matchesMap
+        );
+
+        return [translation, translationWithHighlight];
+    }
+
+    replaceAll(text, matchesMap) { 
+        const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+        return text.replace(re, (matched) => matchesMap[matched.toLowerCase()]);
+    }
+    replaceAllWithHighlight(text, matchesMap) {
+        const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+        return text.replace(re, (matched) => {
+            return `<span class="highlight">${
+                matchesMap[matched.toLowerCase()]
+            }</span>`;
+        });
+    }
+} 
 
 module.exports = Translator;
